7ebc9ceec8981b9ec5fef0dd1b664da40d2907e3
Orden14 <t.lesrel@gmail.com>
2024-10-11 14:48:31 +0200

    Improved navbar

--- src/Components/Nav/Header.jsx
+++ src/Components/Nav/Header.jsx
@@ -7,33 +7,56 @@ function Header() {
     const user = getUserInfos();
 
     return (
-        <div>
-            <ul className="nav">
-                <li className="nav-item">
-                    <Link to={"/welcome"} className={"nav-link"}>
-                        Welcome
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to={"/register"} className={"nav-link"}>
-                        Register
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    {user ? (
-                        <div>
-                            <span role="button" onClick={logout}>
-                                logout
-                            </span>
-                        </div>
-                    ) : (
-                        <Link to={"/login"} className={"nav-link"}>
-                            Login
-                        </Link>
-                    )}
-                </li>
-            </ul>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <Link to={"/"} className="navbar-brand">
+                    MyCv
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li>
+                            <Link to={"/"} className="nav-link">
+                                Voir les CV
+                            </Link>
+                        </li>
+                        {user && (
+                            <li>
+                                <Link to={"/my_cv"} className="nav-link">
+                                    Mes CV
+                                </Link>
+                            </li>
+                        )}
+                    </ul>
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            {user ? (
+                                <Link to={"/profile"} className="nav-link">
+                                    Mon profile
+                                </Link>
+                            ) : (
+                                <Link to={"/register"} className="nav-link">
+                                    S'enregistrer
+                                </Link>
+                            )}
+                        </li>
+                        <li className="nav-item">
+                            {user ? (
+                                <span role="button" className="nav-link" onClick={logout}>
+                                    Déconnexion
+                                </span>
+                            ) : (
+                                <Link to={"/login"} className="nav-link">
+                                    Connexion
+                                </Link>
+                            )}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
