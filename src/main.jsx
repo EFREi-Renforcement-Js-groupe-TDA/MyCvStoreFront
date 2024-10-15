655e690ce9b7a380c2d40e7a7dfbdefdcf2fd0c4
Orden <92043415+Orden14@users.noreply.github.com>
2024-10-15 16:26:46 +0200

    Revert "Added authentication"

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { UserProvider } from "./Context/UserContext";
-import "../public/stylesheet/style.css";
+import "../public/style.css";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <BrowserRouter>
-            <UserProvider>
-                <App />
-            </UserProvider>
-        </BrowserRouter>
+        <BrowserRouter></BrowserRouter>
     </StrictMode>,
 );
