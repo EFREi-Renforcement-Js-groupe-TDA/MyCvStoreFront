bb008b85fb19639aa3ec43c3fdd8026317fabe69
Orden14 <t.lesrel@gmail.com>
2024-10-18 16:17:12 +0200

    remove unused imports and escape character

--- src/Components/Misc/DisclaimerPopup.jsx
+++ src/Components/Misc/DisclaimerPopup.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 const DisclaimerPopup = () => {
     const [showPopup, setShowPopup] = useState(false);
 
@@ -33,15 +33,15 @@ const DisclaimerPopup = () => {
                                 <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
                             </svg>
                             <div>
-                                <span>Il s'agit d'une application déployée à des fins de démonstration, mais les données saisies sont tout de même conservées sur un serveur.</span>
+                                <span>Il s&apos;agit d&apos;une application déployée à des fins de démonstration, mais les données saisies sont tout de même conservées sur un serveur.</span>
                                 <br />
-                                <span>Nous vous prions d'utiliser des informations factices. Toutes les données seront supprimées avant le 30/11/2024.</span>
+                                <span>Nous vous prions d&apos;utiliser des informations factices. Toutes les données seront supprimées avant le 30/11/2024.</span>
                             </div>
                         </div>
                     </div>
                     <div className="text-center mb-3">
                         <button type="button" className="btn btn-secondary" onClick={closePopup}>
-                            J'ai compris
+                            J&apos;ai compris
                         </button>
                     </div>
                 </div>
